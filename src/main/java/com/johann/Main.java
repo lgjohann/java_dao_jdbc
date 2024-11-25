a2ace85453ed3ac625662c62c18a449e4d24f4c6
Guilherme Johann <guilherme4695@gmail.com>
2024-11-25 13:18:41 -0300

    findById implementation

--- src/main/java/com/johann/Main.java
+++ src/main/java/com/johann/Main.java
@@ -1,10 +1,18 @@
 package com.johann;
 
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
+
 public class Main {
     public static void main(String[] args) {
 
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        Seller seller = sellerDao.findById(3);
+
+        System.out.println(seller);
 
 
     }
